8eadbafecb7e04fff4f721e7351e57954b414d41
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-08-08 12:50:32 +0530

    setinterval

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ gsap.from(".line h1",{
     duration: 0.6,
     delay: 0.5
 })
+
+var h5timer = document.querySelector("#line1-part1 h5")
+var grow = 0
+setInterval(function() {
+    if(grow<100){
+        h5timer.innerHTML = grow++
+    }else{
+        h5timer.innerHTML = grow
+    }
+},25)

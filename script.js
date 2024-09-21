2b4d3b685ff37b61f231450367075f5df5c1493d
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-09-21 12:27:41 +0530

    SheryAnimation

--- script.js
+++ script.js
@@ -110,3 +110,12 @@ loadingAnimation();
 cursorAnimation();
 locomotiveAnimation();
 
+function sheryAnimation(){
+  Shery.imageEffect(".image-div",{
+    style:5,
+    debug:true,
+    gooey:true,
+  });
+}
+
+sheryAnimation();
